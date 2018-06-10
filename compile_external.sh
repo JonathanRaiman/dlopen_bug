623393274109304481db546c956513bf91f8e126
Jonathan Raiman <jonathanraiman@gmail.com>
2018-06-10 16:05:45 -0700

    temp fix

--- compile_external.sh
+++ compile_external.sh
@@ -1,1 +1,2 @@
-c++ external_lib.cpp -Wl,-undefined -Wl,dynamic_lookup -o external_lib.so
+c++ -w -Wno-return-type -Wno-undefined-bool-conversion -Wno-potentially-evaluated-expression -fPIC   -o external_lib.o -c external_lib.cpp
+c++ -std=c++11 -O3 -dynamiclib -Wl,-headerpad_max_install_names  -o external_lib.so external_lib.o
